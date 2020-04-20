7df511a5091f679d5627206186513a4d01bbd5b1
mahmoudfrikha <mahmoud.frikha@esprit.tn>
2020-04-21 00:14:50 +0100

    login

---
+++ login/home.php
@@ -0,0 +1,6 @@
+/*To Do: check username and password in Data Base. if authentificated create session and save username and role else redirect to login page */
+<?php
+
+echo $_POST["username"];
+echo $_POST["password"];
+?>
---
+++ login/index.php
@@ -0,0 +1,30 @@
+<?php?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>login</title>
+	<link rel="stylesheet" href="login.css">
+	<link rel="stylesheet" href="icons/fontawesome-free-5.13.0-web/css/all.css">
+</head>
+<body>
+	<form action="home.php" method="post">
+	<div class="bg"></div>
+	<div class="login-box">
+		<h1>Login</h1>
+		<div class="textbox">
+<i class="fas fa-user"></i>
+			<input type="text" placeholder="Username" name="username" value="" required>
+</div>
+<div class="textbox">
+<i class="fas fa-lock"></i>
+			<input type="password" placeholder="Password" name="password" value="" required>
+</div>
+<input class="butt" type="submit" name="" value="Sign in" required>
+<a href="#">Forgot passsword ? </a> <br/>
+<a href="#">Sign up ?</a>
+</div>
+</form>
+</body>
+</html>	
+<?php?>
