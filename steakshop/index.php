d80d60a239ada3003c390302ad297b961e1568f2
mahmoudfrikha <mahmoud.frikha@esprit.tn>
2020-06-08 15:57:50 +0100

    project

--- steakshop/index.php
+++ steakshop/index.php
@@ -44,9 +44,18 @@ if (isset($_SESSION['id']) AND ( $_SESSION['role'] == "client" ))
 		<div class="nav-wraper">
 			<div class="navbar">
 				<ul class="navbar-nav">
-					<li class="nav-item"><a class="nav-link active" href="index.html"><img src="img/header/nav-icon1.png" alt=""> home</a></li>
+					<li class="nav-item"><a class="nav-link active" href="index.php"><img src="img/header/nav-icon1.png" alt=""> home</a></li>
 					<li class="nav-item"><a class="nav-link" href="about-us.html"><img src="img/header/nav-icon2.png" alt="">about</a></li>
-					<li class="nav-item"><a class="nav-link" href="menu.html"><img src="img/header/nav-icon3.png" alt="">menu</a></li>
+					<li class="nav-item submenu dropdown">
+						<a class="nav-link dropdown-toggle active" href="#" data-toggle="dropdown" role="button" aria-haspopup="true"
+						 aria-expanded="false"><img src="img/header/nav-icon3.png" alt="">menu</a>
+						<ul class="dropdown-menu">
+							<li class="nav-item"><a class="nav-link" href="events.php">Events</a></li>
+						
+
+						</ul>
+					</li>
+					
 					<li class="nav-item"><a class="nav-link" href="book-table.html"><img src="img/header/nav-icon4.png" alt="">Book
 							Table</a></li>
 					<li class="nav-item"><a class="nav-link" href="Chefs.html"><img src="img/header/nav-icon5.png" alt="">Chefs</a></li>
@@ -55,6 +64,7 @@ if (isset($_SESSION['id']) AND ( $_SESSION['role'] == "client" ))
 						 aria-expanded="false"><img src="img/header/nav-icon6.png" alt="">Pages</a>
 						<ul class="dropdown-menu">
 							<li class="nav-item"><a class="nav-link" href="elements.html">element</a></li>
+
 						</ul>
 					</li>
 					<li class="nav-item submenu dropdown">
